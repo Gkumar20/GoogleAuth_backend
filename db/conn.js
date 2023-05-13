3bd2c50f2374c056e32404742c09a36c91bd3254
Ganesh Kumar <86399447+Gkumar20@users.noreply.github.com>
2023-05-13 13:56:39 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb://gkumar:ONt8nzj3ID0aegGI@ac-0woxp95-shard-00-00.u8k7yxe.mongodb.net:27017,ac-0woxp95-shard-00-01.u8k7yxe.mongodb.net:27017,ac-0woxp95-shard-00-02.u8k7yxe.mongodb.net:27017/Leads4needs?replicaSet=atlas-232n4e-shard-0&ssl=true&authSource=admin";
+const uri = "mongodb://<username>:<password>@ac-0woxp95-shard-00-00.u8k7yxe.mongodb.net:27017,ac-0woxp95-shard-00-01.u8k7yxe.mongodb.net:27017,ac-0woxp95-shard-00-02.u8k7yxe.mongodb.net:27017/Leads4needs?replicaSet=atlas-232n4e-shard-0&ssl=true&authSource=admin";
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
